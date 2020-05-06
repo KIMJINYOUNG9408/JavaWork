44101fe4b2d3032c5824b8584f583d5f5a082e78
KIMJINYOUNG9408 <kalkungo9408@gmail.com>
2020-05-07 00:23:24 +0900

    2020-05-07-0

---
+++ Lec01_Hello/src/com/lec/java/hello/Korean.java
@@ -0,0 +1,21 @@
+package com.lec.java.hello;
+
+public class Korean {
+    // 필드
+String nation = "대한민국";
+String name;
+String ssn;                              // 생성자
+    /*public Korean(String n, String s){
+     * name = n;
+     * ssn = s;
+     */
+
+
+public Korean(String name, String ssn)
+{
+this.name = name;                    
+this.ssn = ssn;        // 필드와 매개변수를 구분하기 위해 this를 사용
+// this : 객체 자신을 뜻함
+}
+
+}
