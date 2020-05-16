f673f03ac13b9fa5fb2b5e67663431da5e00d269
KIMJINYOUNG9408 <kalkungo9408@gmail.com>
2020-05-16 18:51:30 +0900

    2020-05-16-0

--- Lec_WEB/WebContent/myweb/index.js
+++ Lec_WEB/WebContent/myweb/index.js
@@ -9,29 +9,29 @@ $(document).ready(function(){
 });
 
 
-// 클릭했을때 1번~10번까지 확대시키고 변경시키려는 func
-function ranksee(i){
-    var name = "rank" + i;
-    var name2 = "rank" + i + "_img";
-    var name3 = "rank" + i + "_p";
-    document.getElementById(name).style.height = "100px";   
-    document.getElementById(name2).style.height = "100px"; 
-    document.getElementById(name).style.fontSize = "20px";
-    document.getElementById(name3).innerHTML = "twice ";
+// // 클릭했을때 1번~10번까지 확대시키고 변경시키려는 func
+// function ranksee(i){
+//     var name = "rank" + i;
+//     var name2 = "rank" + i + "_img";
+//     var name3 = "rank" + i + "_p";
+//     document.getElementById(name).style.height = "100px";   
+//     document.getElementById(name2).style.height = "100px"; 
+//     document.getElementById(name).style.fontSize = "20px";
+//     document.getElementById(name3).innerHTML = "twice ";
        
-}
+// }
 
-// 클릭했을때 1번~10번까지 확대된걸 다시 되돌리려는 func
-function rankhind(i){
-    var name = "rank" + i;
-    var name2 = "rank" + i + "_img";
-    var name3 = "rank" + i + "_p";
-    document.getElementById(name).style.height = "50px";   
-    document.getElementById(name2).style.height = "50px"; 
-    document.getElementById(name).style.fontSize = "15px";
-    document.getElementById(name3).innerHTML = "heart";
+// // 클릭했을때 1번~10번까지 확대된걸 다시 되돌리려는 func
+// function rankhind(i){
+//     var name = "rank" + i;
+//     var name2 = "rank" + i + "_img";
+//     var name3 = "rank" + i + "_p";
+//     document.getElementById(name).style.height = "50px";   
+//     document.getElementById(name2).style.height = "50px"; 
+//     document.getElementById(name).style.fontSize = "15px";
+//     document.getElementById(name3).innerHTML = "heart";
        
-}
+// }
 
  // 실검 ajax func
 $(document).ready(function(){
@@ -73,35 +73,114 @@ if(i>20){
 }
 
 $(document).ready(function() {
-    //사용할 배너
+
     var $banner = $(".banner").find("ul");
 
-    var $bannerWidth = $banner.children().outerWidth();//배너 이미지의 폭
-    var $bannerHeight = $banner.children().outerHeight(); // 높이
-    var $bannerLength = $banner.children().length;//배너 이미지의 갯수
+    var $bannerWidth = $banner.children().outerWidth();
+    var $bannerHeight = $banner.children().outerHeight(); 
+    var $bannerLength = $banner.children().length;
     var rollingId;
 
-    //정해진 초마다 함수 실행
-    rollingId = setInterval(function() { rollingStart(); }, 3000);//다음 이미지로 롤링 애니메이션 할 시간차
-
+   
+    rollingId = setInterval(function() { rollingStart(); }, 3000);
 
     
     function rollingStart() {
         $banner.css("width", $bannerWidth * $bannerLength + "px");
         $banner.css("height", $bannerHeight + "px");
-        //alert(bannerHeight);
-        //배너의 좌측 위치를 옮겨 준다.
-        $banner.animate({left: - $bannerWidth + "px"}, 1500, function() { //숫자는 롤링 진행되는 시간이다.
-            //첫번째 이미지를 마지막 끝에 복사(이동이 아니라 복사)해서 추가한다.
-            $(this).append("<li>" + $(this).find("li:first").html() + "</li>");
-            //뒤로 복사된 첫번재 이미지는 필요 없으니 삭제한다.
-            $(this).find("li:first").remove();
-            //다음 움직임을 위해서 배너 좌측의 위치값을 초기화 한다.
+        
+        $banner.animate({left: - $bannerWidth + "px"}, 1500, function() { 
+            $(this).append("<li>" + $(this).find("li:first").html() + "</li>");            
+            $(this).find("li:first").remove();          
             $(this).css("left", 0);
-            //이 과정을 반복하면서 계속 롤링하는 배너를 만들 수 있다.
+            
         });
     }
 }); 
+
+$(document).ready(function(){
+		 
+    // JSON
+    url = "chart_rankmain.json"
+    $.ajax({
+        url : url, 
+        type : "GET",
+        cache : false,
+        success : function(data, status){
+            if(status == "success") parseSON(data);
+        }
+    });		
+ 
+});
+
+function parseSON(jsonObj) {
+var row = jsonObj.chart_rank.row;
+var music_10 = new Array();
+
+
+
+
+music_10[0] = "<tr style='height: 50px;'><th style='width: 5%;'>R</th><th style='width: 10%;'>앨범</th>";	
+music_10[0] += "<th style='width: 20%;'>가수</th><th style='width: 45%;'>제목</th><th style='width: 10%;'>Heart</th><th style='width: 10%;'>Play</th></tr>" 
+music_10[1] = "<tr style='height: 50px;'><th style='width: 5%;'>R</th><th style='width: 10%;'>앨범</th>";	
+music_10[1] += "<th style='width: 20%;'>가수</th><th style='width: 45%;'>제목</th><th style='width: 10%;'>Heart</th><th style='width: 10%;'>Play</th></tr>" 
+
+
+for (i = 0; i < row.length; i++) {
+    if(i<10){
+   music_10[0] += "<tr>"; 
+   music_10[0] += "<td>"+ (i+1) + "</td>";
+   music_10[0] += "<td style='width: 10%; height:50px'> <img src='" + row[i].Image  +  "' width='100%' height='100%' alt='' style='float: left; padding: 0px; margin: 0px; margin-top: 7px;'></td>";
+   music_10[0] += "<td>" + row[i].Artist + "</td>";
+   music_10[0] += "<td>" + row[i].Name + "</td>";
+   
+   music_10[0] += "<td>" + "<i class='fas fa-heart' id='heart" + (i+1)  + "'style= 'color:gray;' ondblclick=" 
+       + '"'
+       + "if(document.getElementById('heart" + (i+1) + "').style.color == 'gray'){"                   
+       + "this.style.color = 'red'}"
+       + "else{ this.style.color = 'gray'}"
+       + '"'
+       +"></i>" + "</td>";
+       music_10[0] += "<td>" + "<i class='fas fa-play search_button'></i>" + "</td>";
+       music_10[0] += "</tr>"; 
+    } else {
+        music_10[1] += "<tr>"; 
+        music_10[1] += "<td>"+ (i+1)  + "</td>";
+        music_10[1] += "<td style='width: 10%; height:50px'> <img src='" + row[i].Image  +  "' width='100%' height='100%' alt='' style='float: left; padding: 0px; margin: 0px; margin-top: 7px;'></td>";
+        music_10[1] += "<td>" + row[i].Artist + "</td>";
+        music_10[1] += "<td>" + row[i].Name + "</td>";
+        
+        music_10[1] += "<td>" + "<i class='fas fa-heart' id='heart" + (i+1)  + "'style= 'color:gray;' ondblclick=" 
+            + '"'
+            + "if(document.getElementById('heart" + (i+1) + "').style.color == 'gray'){"                   
+            + "this.style.color = 'red'}"
+            + "else{ this.style.color = 'gray'}"
+            + '"'
+            +"></i>" + "</td>";
+            music_10[1] += "<td>" + "<i class='fas fa-play search_button'></i>" + "</td>";
+            music_10[1] += "</tr>";  
+    }
+
+    $("#chart20_main").html(music_10[0])
+}   
+
+var i = 1;
+var interval = setInterval(function(){
+    	
+    if(i>1){		
+     i=0;		
+    }
+         $("#chart20_main").html(music_10[i])
+     i++;	  
+    }, 4000);
+     
+
+  
+
+
+
+}
+
 
 
 
